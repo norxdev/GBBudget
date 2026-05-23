c6ed3935e7263a897257b37e89e1b2eef8e9f62b
norxdev <norxonics@gmail.com>
2026-05-23 06:54:30 -0400

    Update index.ts

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -16,8 +16,8 @@ serve(async (req) => {
   }
 
   const supabase = createClient(
-    Deno.env.get('SUPABASE_URL') ?? '',
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+    Deno.env.get('APP_SUPABASE_URL') ?? '',
+    Deno.env.get('SERVICE_ROLE_KEY') ?? ''
   )
 
   switch (event.type) {
